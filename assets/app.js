17a9a31923c65af497d8854b0de70928a369a348
TF23689 <mtsfw48254@privaterelay.appleid.com>
2026-08-31 21:56:27 +0800

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -309,4 +309,9 @@
       showToast("出咗少少問題…", "你可以截圖畀我，我幫你執");
     }
   });
-})();
+})();document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    showToast("載入任務中…", "如果你見到呢句＝JS 正常運作");
+    await initMissionsPage();
+    await initDayPage();
+  } catch (e) {
